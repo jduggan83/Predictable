6370244ed2f6ddafc3ae4124210aa6232ad52453
Jonathan Duggan <jduggan_contractor@decaresystems.ie>
2016-08-12 12:24:37 +0100

    upgraded onsenui to latest

--- app/www/controllers.js
+++ app/www/controllers.js
@@ -3,50 +3,20 @@
  */
 var controllers = angular.module('app.controllers', []);
 
-controllers.controller('PageController', function($scope) {
+controllers.controller('PageController', ['$scope', function($scope) {
     ons.ready(function() {
         // Init code here
-        //onclick="myNavigator.pushPage('components/match-details/view.html', { 'animation': 'slide' })"
     });
 
     $scope.pop = function(){
         console.log("dd");
     };
-});
+}]);
 
-controllers.controller('MatchController', function($scope, $q, $http) {
-    $scope.MyDelegate = {
-        configureItemScope: function(index, itemScope) {
-            if (!itemScope.item) {
-                console.log("Created item #" + index);
-                itemScope.canceler = $q.defer();
+controllers.controller('MatchController', ['$scope', 'matchService', function($scope, matchService) {
+    $scope.matches = [];
 
-                itemScope.item = {
-                    title: 'Item #' + (index + 1),
-                    label: '',
-                    desc: '',
-                    rand: Math.random()
-                };
-                $http.get('https://baconipsum.com/api/?type=meat-and-filler&sentences=1', {
-                    timeout: itemScope.canceler.promise
-                }).success(function(data) {
-                    itemScope.item.desc = data[0];
-                    itemScope.item.label = itemScope.item.desc.substr(0, itemScope.item.desc.indexOf(" ")) + 'bacon'
-                }).error(function() {
-                    itemScope.item.desc = 'No bacon lorem ipsum';
-                    itemScope.item.label = 'No bacon'
-                });
-            }
-        },
-        calculateItemHeight: function(index) {
-            return 91;
-        },
-        countItems: function() {
-            return 10000000;
-        },
-        destroyItemScope: function(index, itemScope) {
-            itemScope.canceler.resolve();
-            console.log("Destroyed item #" + index);
-        }
-    };
-});
+    matchService.list().then(function(result){
+        $scope.matches = result.data.data;
+    });
+}]);
